da5e0b2c4bf0e8bd1e276008a6ccf1ca203d2bd8
Charly Chacon <charlyjchaconc@gmail.com>
2022-11-23 21:45:13 -0500

    second

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,36 @@
+"use client"
+import gsap from "gsap"
+import { useEffect } from "react"
+import styles from "../styles/landing.module.css"
+
 export default function HomePage () {
+
+    // const animation = () => {
+    //     let items = document.querySelector(".texto")
+    //     gsap.from(items, {
+    //         opacity: 0.5, duration: 1
+    //     })
+    // }
+
+    // useEffect(()=>{
+    //     animation()
+    // }, [])
+
     return <>
-    <div className="container w-100 text-center p-5 m-4">
-        <div className="row w-100">
-            <div className="col-12 p-3 m-2 texto">ABOUT</div>
+    {/* <div className="container w-100 landing">
+        <div className="row">
+            <div className={`col-xs-10 p-3 text-center m-2 texto`}>ABOUT
+            </div>
         </div>
         <div className="row w-100">
-            <div className="col-12 p-3 m-2 texto">PROJECTS</div>
+            <div className="col-12 p-3 text-center m-2 texto">PROJECTS</div>
         </div>
         <div className="row w-100">
-            <div className="col-12 p-3 m-2 texto">CONTACT</div>
+            <div className="col-12 text-center p-3 m-2 texto">CONTACT</div>
         </div>
+    </div> */}
+    <div className="row">
+        <div className="col-5 col-lg-12 text-center">HOLA</div>
     </div>
     </>
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,14 +6,16 @@ const links = [{label: "Home", route: "/"}, {label: "About", route: "/about",}]
 
 export default function Navbar () {
     return <>
-    <nav className="w-100 shadow-lg rounded">
-        {links.map(({label, route}) => 
+    <nav className="w-100 shadow-lg rounded d-flex justify-content-between">
+        {/* {links.map(({label, route}) => 
             <li className="btn btn-outline boton" key={label}>
                 <Link className="text-light" href={route}>
                     {label}
                 </Link>
             </li>
-        )}
+        )} */}
+        <div className="float-start">Charlytoc</div>
+        <div className="float-end"><ion-icon name="menu-outline"></ion-icon></div>
     </nav>
     </>
 }
